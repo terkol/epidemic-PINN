4205bf080ed2890d83681173650eb2d9bb77c1e3
terkol <tero0408@gmail.com>
2026-06-01 18:40:41 +0300

    Improved variable names

--- src/pinn/inverse_sir.py
+++ src/pinn/inverse_sir.py
@@ -1,20 +1,21 @@
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 from data_loader import *
 
 class PINN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.n = nn.Sequential(
-            nn.Linear(1,16), 
+        self.net = nn.Sequential(
+            nn.Linear(1,64), 
             nn.Tanh(), 
-            nn.Linear(16, 3), 
+            nn.Linear(64, 3), 
             nn.Softmax(1))
-        self.b = nn.Parameter(torch.tensor([0.5]))
-        self.g = nn.Parameter(torch.tensor([0.5]))
+        self.beta = nn.Parameter(torch.tensor([0.5]))
+        self.gamma = nn.Parameter(torch.tensor([0.5]))
 
     def forward(self, x):
-        y = self.n(x)
+        y = self.net(x)
         return y[:,0:1], y[:,1:2], y[:,2:3]
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -27,34 +28,33 @@ y3 = d[3].to(device)
 
 t = len(x)
 model = PINN().to(device)
-optimizer = torch.optim.Adam(model.parameters())
+opt = torch.optim.Adam(model.parameters())
 
 for i in range(10000):
-    optimizer.zero_grad()
     x.requires_grad = True
     
     p1, p2, p3 = model(x)
     
-    l1 = nn.MSELoss()(p2, y2) + nn.MSELoss()(p3, y3)
+    l1 = F.mse_loss(p2, y2) + F.mse_loss(p3, y3)
 
     d1 = torch.autograd.grad(p1, x, torch.ones_like(p1), True)[0]
     d2 = torch.autograd.grad(p2, x, torch.ones_like(p2), True)[0]
     d3 = torch.autograd.grad(p3, x, torch.ones_like(p3), True)[0]
     
-    r1 = d1 - (-t * model.b * p1 * p2)
-    r2 = d2 - (t * model.b * p1 * p2 - t * model.g * p2)
-    r3 = d3 - (t * model.g * p2)
+    r1 = d1 - (-t * model.beta * p1 * p2)
+    r2 = d2 - (t * model.beta * p1 * p2 - t * model.gamma * p2)
+    r3 = d3 - (t * model.gamma * p2)
     
     l2 = torch.mean(r1**2) + torch.mean(r2**2) + torch.mean(r3**2)
     
-    L = l1 + (1e-6 * l2)
+    loss = l1 + (1e-6 * l2)
 
-    L.backward()
-    optimizer.step()
+    loss.backward()
+    opt.step()
+    opt.zero_grad()
     
     if i % 1000 == 0:
-        print(f"Epoch: {i} | Loss: {L.item()}")
+        print(f"Epoch: {i} | Loss: {loss.item()}")
 
-print("Done")
-print(f"Beta: {model.b.item()}")
-print(f"Gamma: {model.g.item()}")
+print(f"Beta: {model.beta.item()}")
+print(f"Gamma: {model.gamma.item()}")
